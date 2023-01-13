2fcf845d6cfc715dd3dfd51872f76dab19a1359e
webdev-sammy <106578070+webdev-sammy@users.noreply.github.com>
2023-01-13 13:51:14 +1100

    Update Valid Parentheses.js

--- 5kyu/Valid Parentheses.js
+++ 5kyu/Valid Parentheses.js
@@ -1,14 +1,14 @@
-Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
-Examples
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+// Examples
 
-"()"              =>  true
-")(()))"          =>  false
-"("               =>  false
-"(())((()())())"  =>  true
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
 
-Constraints
+// Constraints
 
-0 <= input.length <= 100
+// 0 <= input.length <= 100
 
 function validParentheses(parens) {
   let n = 0
